06e95154b2a779b72374932804e747322b62b287
yilengyao <yilengyao@gmail.com>
2023-08-28 23:22:20 +0000

    first commit

---
+++ src/test/java/io/github/yilengyao/dummy/DummyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yilengyao.dummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
